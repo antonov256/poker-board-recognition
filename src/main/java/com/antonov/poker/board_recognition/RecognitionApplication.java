a22661af3517b5593c7df710d88daa320038342a
Георгий Антонов <gogiant1992@gmail.com>
2021-05-16 00:51:12 +0300

    Avoid static, improvements
    RecognitionApplication -> RecognitionTask
    Main -> RecognitionApplication

--- src/main/java/com/antonov/poker/board_recognition/RecognitionApplication.java
+++ src/main/java/com/antonov/poker/board_recognition/RecognitionApplication.java
@@ -1,84 +1,78 @@
 package com.antonov.poker.board_recognition;
 
+import com.antonov.poker.board_recognition.config.RecognitionApplicationConfiguration;
+import com.antonov.poker.board_recognition.config.loading.*;
+import com.antonov.poker.board_recognition.config.parsing.ColorResourceBundleParser;
+import com.antonov.poker.board_recognition.config.parsing.CropResourceBundleParser;
+import com.antonov.poker.board_recognition.config.parsing.ResourceBundleParser;
+import com.antonov.poker.board_recognition.image_processing.ImageProcessor;
+import com.antonov.poker.board_recognition.poker.model.CardRank;
+import com.antonov.poker.board_recognition.poker.model.CardSuit;
 import com.antonov.poker.board_recognition.recognition.BoardRecognition;
-import com.antonov.poker.board_recognition.recognition.model.Image;
-import com.antonov.poker.board_recognition.recognition.model.SimpleImage;
-import com.antonov.poker.board_recognition.poker.model.Board;
+import com.antonov.poker.board_recognition.recognition.model.CardMarkup;
+import com.antonov.poker.board_recognition.recognition.model.Crop;
+import com.antonov.poker.board_recognition.recognition.model.CropConfig;
+import com.antonov.poker.board_recognition.recognition.model.TemplatesContainer;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.awt.*;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.ResourceBundle;
 
 public class RecognitionApplication {
-    private final String imagesPath;
-    private final BoardRecognition boardRecognition;
+    private final String imagesDirPath;
 
-    private final File unrecognizedDir;
-
-    private int correctCount;
-    private int totalCount;
-
-    public RecognitionApplication(String imagesPath, BoardRecognition boardRecognition) {
-        this.imagesPath = imagesPath;
-        this.boardRecognition = boardRecognition;
-
-        unrecognizedDir = new File(imagesPath, "unrecognized " + System.currentTimeMillis());
+    public RecognitionApplication(String imagesDirPath) {
+        this.imagesDirPath = imagesDirPath;
     }
 
-    public String start() {
-        File dir = new File(imagesPath);
-
-        if(!dir.exists() || !dir.isDirectory())
-            return "!dir.exists() || !dir.isDirectory(). dir = " + dir;
-
-        File[] files = dir.listFiles();
-        if(files == null)
-            return "files == null";
-
-        for(File f : files) {
-            if(f == null || !f.exists() || !f.getName().endsWith(".png"))
-                continue;
-
-            try {
-                BufferedImage bufferedImage = ImageIO.read(f);
-                Image image = new SimpleImage(bufferedImage);
-
-                Board recognizedBoard = boardRecognition.recognize(image);
-
-                String fileStr = f.getName().replaceAll("\\.png", "");
-                Board fileBoard = new Board.Utils().fromString(fileStr);
-
-                System.out.println(f.getName() + " - " + recognizedBoard.toString());
-
-                if(recognizedBoard.equals(fileBoard)) {
-                    correctCount++;
-                } else {
-                    System.out.println("## ERROR " + f.getName());
-                    copyUnrecognizedImage(f);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                copyUnrecognizedImage(f);
-            }
-
-            totalCount++;
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("You need to specify images path as argument");
+            return;
         }
+
+        String imagesDirPath = args[0];
+        RecognitionApplication recognitionApplication = new RecognitionApplication(imagesDirPath);
+        recognitionApplication.launch();
+    }
+
+    public void launch() {
+        RecognitionTask recognitionTask = new RecognitionTask(
+                new File(imagesDirPath),
+                configureBoardRecognition()
+        );
+
+        recognitionTask.run();
+        int correctCount = recognitionTask.getCorrectCount();
+        int totalCount = recognitionTask.getTotalCount();
 
         String summary = "Correct: " + correctCount + " / " + totalCount + " (" + correctCount * 100d / totalCount + "%)";
-        return summary;
+        System.out.println(summary);
     }
 
-    private void copyUnrecognizedImage(File file) {
-        if(!unrecognizedDir.exists()) {
-            unrecognizedDir.mkdir();
-        }
+    private BoardRecognition configureBoardRecognition() {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+        String templatesDirPath = resourceBundle.getString("templates.dir");
 
-        try {
-            Files.copy(file.toPath(), new File(unrecognizedDir, file.getName()).toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ResourceBundleParser<Crop> cropParser = new CropResourceBundleParser();
+        ResourceBundleParser<Color> colorParser = new ColorResourceBundleParser();
+
+        Loading<CropConfig> cropConfigLoading = new CropConfigLoading("crop_config", cropParser);
+        Loading<ImageProcessor> preprocessorLoading = new ImageColorReplacingPreprocessorLoading("recognition", colorParser);
+        Loading<CardMarkup> cardMarkupLoading = new CardMarkupLoading("card_markup", cropParser);
+        Loading<TemplatesContainer<CardRank>> rankTemplatesLoading = new CardRankTemplatesLoading(new File(templatesDirPath, "ranks"));
+        Loading<TemplatesContainer<CardSuit>> suitTemplatesLoading = new CardSuitTemplatesLoading(new File(templatesDirPath, "suits"));
+        Loading<RecognitionSettings> recognitionSettingsLoading = new RecognitionSettingsLoading("recognition");
+
+        RecognitionApplicationConfiguration recognitionApplicationConfiguration = new RecognitionApplicationConfiguration(
+                cropConfigLoading,
+                preprocessorLoading,
+                cardMarkupLoading,
+                rankTemplatesLoading,
+                suitTemplatesLoading,
+                recognitionSettingsLoading
+        );
+
+        return recognitionApplicationConfiguration.getBoardRecognition();
     }
 }
---
+++ src/main/java/com/antonov/poker/board_recognition/RecognitionTask.java
@@ -0,0 +1,96 @@
+package com.antonov.poker.board_recognition;
+
+import com.antonov.poker.board_recognition.poker.model.Board;
+import com.antonov.poker.board_recognition.recognition.BoardRecognition;
+import com.antonov.poker.board_recognition.recognition.model.Image;
+import com.antonov.poker.board_recognition.recognition.model.SimpleImage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class RecognitionTask {
+    private final File imagesDir;
+    private final BoardRecognition boardRecognition;
+
+    private final File unrecognizedDir;
+
+    private int correctCount;
+    private int totalCount;
+
+    public RecognitionTask(File imagesDir, BoardRecognition boardRecognition) {
+        this.imagesDir = imagesDir;
+        this.boardRecognition = boardRecognition;
+
+        unrecognizedDir = new File(imagesDir, "unrecognized " + System.currentTimeMillis());
+    }
+
+    public void run() {
+        if (!imagesDir.exists()) {
+            System.err.println("imagesDir not exists: " + imagesDir);
+            return;
+        }
+
+        if (!imagesDir.isDirectory()) {
+            System.err.println("imagesDir is not directory: " + imagesDir);
+            return;
+        }
+
+        File[] files = imagesDir.listFiles();
+        if (files == null) {
+            System.err.println("files == null. imagesDir: " + imagesDir);
+            return;
+        }
+
+        for (File f : files) {
+            if (f == null || !f.exists() || !f.getName().endsWith(".png"))
+                continue;
+
+            try {
+                BufferedImage bufferedImage = ImageIO.read(f);
+                Image image = new SimpleImage(bufferedImage);
+
+                Board recognizedBoard = boardRecognition.recognize(image);
+
+                String fileStr = f.getName().replaceAll("\\.png", "");
+                Board fileBoard = new Board.Utils().fromString(fileStr);
+
+                System.out.println(f.getName() + " - " + recognizedBoard.toString());
+
+                if (recognizedBoard.equals(fileBoard)) {
+                    correctCount++;
+                } else {
+                    System.err.println("## ERROR ## Wrong recognition for file: " + f.getName());
+                    copyUnrecognizedImage(f);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                copyUnrecognizedImage(f);
+            }
+
+            totalCount++;
+        }
+    }
+
+    private void copyUnrecognizedImage(File file) {
+        if (!unrecognizedDir.exists()) {
+            unrecognizedDir.mkdir();
+        }
+
+        try {
+            Files.copy(file.toPath(), new File(unrecognizedDir, file.getName()).toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getCorrectCount() {
+        return correctCount;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+}
